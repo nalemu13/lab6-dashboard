98bf899e0eb1c8d566ca5c6c35bb29b03e1f05ed
Naod M Alemu <nalemu13@uw.edu>
2026-02-19 17:49:17 -0800

    Set up Mapbox map with collision data layer
    Initialize Mapbox map and add collision data layer.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmFsZW11MTMiLCJhIjoiY21sMGR1OHlkMGNsMDNpcHhqbHQxZnNkaSJ9.lIr1YapCLUudIRiNX6kFuA';
 
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v11',
+center: [-122.33, 47.60],
+zoom: 11
+});
+
+
+map.on('load', () => {
+
+fetch('data/collisions.geojson')
+
+.then(response => response.json())
+
+.then(data => {
+
+map.addSource('collisions', {
+
+type: 'geojson',
+
+data: data
+
+});
+
+
+map.addLayer({
+
+id: 'collisions-layer',
+
+type: 'circle',
+
+source: 'collisions',
+
+paint: {
+
+'circle-radius': 4,
+
+'circle-color': '#ff6600',
+
+'circle-opacity': 0.7
+
+}
+
+});
+
+});
+
+});
